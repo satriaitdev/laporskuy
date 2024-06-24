39a250f2440e27cb3c284c31c2cc7e1aaf752129
dimyatimaulana <dimyatimaulana@gmail.com>
2024-06-24 21:09:42 +0700

    feat: ui jumbotron, about us, form

--- src/rust_icp_frontend/tailwind.config.js
+++ src/rust_icp_frontend/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const { transform } = require('typescript');
+
 // tailwind.config.js
 module.exports = {
   content: [
@@ -6,7 +9,20 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,scss}", // include scss files
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 50s linear infinite"
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" }
+        }
+      },
+      backgroundImage: {
+        'world-map': "url('./src/images/Map.png')",
+      }
+    },
   },
   plugins: [],
 };
